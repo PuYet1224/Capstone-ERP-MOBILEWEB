ca53ef75079147a141c59018a33d8ecd3e402620
nguyentanphu1224 <nguyentanphu1224@gmail.com>
2026-03-15 18:06:55 +0700

    Fix Vercel: correct output dir + prod env + vercel.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   productionID: 3,
-  apiServer: 'http://api.hoaiminh.vn',
+  apiServer: 'http://puyet1224-001:Puyet1224%40@puyet1224-001-site1.jtempurl.com',
   identityServer: 'http://identity.hoaiminh.vn'
 };
