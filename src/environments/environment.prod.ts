742e6586e20e743b07edc479b7aec5a8c5bbec7d
nguyentanphu1224 <nguyentanphu1224@gmail.com>
2026-03-18 10:42:28 +0700

    feat: config Vercel reverse proxy to bypass mixed-content error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   productionID: 3,
-  apiServer: 'http://puyet1224-001:Puyet1224%40@puyet1224-001-site1.jtempurl.com',
-  identityServer: 'http://identity.hoaiminh.vn'
+  apiServer: '/proxy-api',
+  identityServer: '/proxy-identity'
 };
