4f7cd586e4a487e78832d32ceea155eeac851a62
nguyentanphu1224 <nguyentanphu1224@gmail.com>
2026-04-15 17:55:57 +0700

    no message

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   productionID: 3,
+  productID: 3,
   apiServer: 'http://puyet1224-001:Puyet1224%40@puyet1224-001-site1.jtempurl.com',
   identityServer: 'http://identity.hoaiminh.vn'
 };
